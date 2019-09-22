4bde2ce078c7895618952469aef7029bd1cdb9ef
js-tut <johnsmilga@gmail.com>
2019-09-22 13:12:56 -0700

    display single restaurant page

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { firestore } from "../firebase";
+const Restaurant = props => {
+  const id = props.match.params.id;
 
-const Restaurant = () => {
-  return <div>Single Restaurant Page</div>;
+  const [restaurant, SetRestaurant] = useState(null);
+  useEffect(() => {
+    const singleRef = firestore.doc(`restaurants/${id}`);
+    // console.log(singleRef);
+    singleRef
+      .get()
+      .then(snapshot => {
+        console.log(snapshot);
+
+        const id = snapshot.id;
+        SetRestaurant({ id, ...snapshot.data() });
+      })
+      .catch(error => console.log(error));
+    // eslint-disable-next-line
+  }, []);
+  if (!restaurant) {
+    return <h1>Loading.....</h1>;
+  }
+  return (
+    <div>
+      <h2>{restaurant.title}</h2>
+    </div>
+  );
 };
 
 export default Restaurant;
