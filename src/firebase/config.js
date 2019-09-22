35fc0d895173d00da875875a0a81bab094db4caa
John <johnsmilga@gmail.com>
2019-09-22 16:57:03 -0700

    more testing

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyCCqHvI4LcJ2o2kQMPGPYAuH40P5DZ7Imo",
-  authDomain: "foodie-b1b5f.firebaseapp.com",
-  databaseURL: "https://foodie-b1b5f.firebaseio.com",
-  projectId: "foodie-b1b5f",
-  storageBucket: "",
-  messagingSenderId: "853621967525",
-  appId: "1:853621967525:web:1e0f69231d8a4b74669b4c"
-};
