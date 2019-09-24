788651bdd8ddc38ea55baea2fb24aac2748ce0d7
js-tut <johnsmilga@gmail.com>
2019-09-24 13:36:34 -0700

    user validation added

--- src/UserContext.js
+++ src/UserContext.js
@@ -1,30 +1,43 @@
 import React, { useState, useEffect, useRef } from "react";
-import { auth } from "./firebase";
-
+import { auth, firestore } from "./firebase";
 const UserContext = React.createContext();
 export default function UserProvider(props) {
   const [user, setUser] = useState(null);
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const unsubscribe = useRef(null);
   useEffect(() => {
-    unsubscribe.current = auth.onAuthStateChanged(user => {
+    unsubscribe.current = auth.onAuthStateChanged(async user => {
       if (user) {
         const { uid, displayName, email } = user;
-        setUser({ uid, displayName, email });
+        const currentUser = await SetupAndGetUser({
+          uid,
+          displayName,
+          email
+        }).catch(e => console.log(e));
+        setUser(currentUser);
       } else {
         setUser(null);
       }
     });
-    return () => {
-      unsubscribe.current();
-    };
-  });
+
+    return () => unsubscribe.current();
+  }, []);
   const handleOpenSidebar = () => {
     setSidebarOpen(true);
   };
   const handleCloseSidebar = () => {
     setSidebarOpen(false);
   };
+  const SetupAndGetUser = async user => {
+    let userRef = firestore.doc(`users/${user.uid}`);
+    let currentUser = await userRef.get();
+    if (!currentUser.exists) {
+      await userRef.set(user).catch(e => console.log(e));
+      currentUser = await userRef.get();
+    }
+    return { id: currentUser.id, ...currentUser.data() };
+  };
+
   return (
     <UserContext.Provider
       value={{ user, sidebarOpen, handleOpenSidebar, handleCloseSidebar }}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,6 @@ const Home = () => {
         });
         setRestaurants(restaurants);
       });
-    // console.log(unsubscribe);
     return () => {
       unsubscribe();
     };
